a18812cce9e441e536c45b4465b4c13b70ed4726
Bom <11377ny@gmail.com>
2022-05-24 09:35:03 -0400

    Adjust CSS for coloration

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
 // Set random color background
-const colors = [
-    "#d5d6ea",
-    "#f6f6eb",
-    "#f5d5cb",
-    "#f6ecf5",
-    "#f6ecf5",
-    "#f3ddf2",
-    "#FFDFD3",
-    "#D291BC",
-    "#D5D6EA",
-    "#F6F6EB",
-    "#D7ECD9",
-    "#F5D5CB",
-    "#F6ECF5",
-    "#F3DDF2",
-];
+// const colors = [
+//     "#fec5bb",
+//     "#fcd5ce",
+//     "#fae1dd",
+//     "#d8e2dc",
+//     "#ece4db",
+//     "#ffe5d9",
+//     "#f8edeb",
+//     "#ffd7ba",
+//     "#e8e8e4",
+//     "#fec89a",
+// ];
 
-randColor1 = colors[Math.floor(Math.random() * colors.length)];
-randColor2 = colors[Math.floor(Math.random() * colors.length)];
-document.body.style.backgroundImage = `linear-gradient(90deg, ${randColor1}, ${randColor2})`;
+// randColor1 = colors[Math.floor(Math.random() * colors.length)];
+// randColor2 = colors[Math.floor(Math.random() * colors.length)];
+// document.body.style.backgroundImage = `linear-gradient(90deg, ${randColor1}, ${randColor2})`;
 
 
 // Set a clickable works to show the details
