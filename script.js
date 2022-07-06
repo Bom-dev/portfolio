6a66580ff31eee102d697af346983b5be6f8b042
Bom <11377ny@gmail.com>
2022-07-06 16:37:42 -0400

    Apply D's advice

--- script.js
+++ script.js
@@ -1,28 +1,16 @@
-// Set random color background
-// const colors = [
-//     "#fec5bb",
-//     "#fcd5ce",
-//     "#fae1dd",
-//     "#d8e2dc",
-//     "#ece4db",
-//     "#ffe5d9",
-//     "#f8edeb",
-//     "#ffd7ba",
-//     "#e8e8e4",
-//     "#fec89a",
-// ];
-
-// randColor1 = colors[Math.floor(Math.random() * colors.length)];
-// randColor2 = colors[Math.floor(Math.random() * colors.length)];
-// document.body.style.backgroundImage = `linear-gradient(90deg, ${randColor1}, ${randColor2})`;
-
-
 // Set a clickable works to show the details
+
 const work1 = document.getElementById("work1");
 const work2 = document.getElementById("work2");
 const work3 = document.getElementById("work3");
 const work4 = document.getElementById("work4");
 const work5 = document.getElementById("work5");
+
+const img1 = document.getElementById("img1")
+const img2 = document.getElementById("img2")
+const img3 = document.getElementById("img3")
+const img4 = document.getElementById("img4")
+const img5 = document.getElementById("img5")
 
 work1.addEventListener("click", () => {
     const detail1 = document.getElementById("detail1");
@@ -45,6 +33,31 @@ work4.addEventListener("click", () => {
 });
 
 work5.addEventListener("click", () => {
+    const detail5 = document.getElementById("detail5");
+    detail5.classList.toggle("hidden");
+});
+
+img1.addEventListener("click", () => {
+    const detail1 = document.getElementById("detail1");
+    detail1.classList.toggle("hidden");
+});
+
+img2.addEventListener("click", () => {
+    const detail2 = document.getElementById("detail2");
+    detail2.classList.toggle("hidden");
+});
+
+img3.addEventListener("click", () => {
+    const detail3 = document.getElementById("detail3");
+    detail3.classList.toggle("hidden");
+});
+
+img4.addEventListener("click", () => {
+    const detail4 = document.getElementById("detail4");
+    detail4.classList.toggle("hidden");
+});
+
+img5.addEventListener("click", () => {
     const detail5 = document.getElementById("detail5");
     detail5.classList.toggle("hidden");
 });
